3d3ea49267954eaf290d7eab3acb12c90cc21a8e
Joshua <jdrego@asu.edu>
2021-05-06 17:28:04 -0700

    bug fix

--- imageSTEAM/utils.py
+++ imageSTEAM/utils.py
@@ -13,10 +13,10 @@ def display_img(a, title = "Original"):
 def crop(image, display=True):
   h, w = image.shape[:2]
   print("Cropping image with original dimensions: \n{} rows x {} columns".format(h, w))
-  h0 = int(input("Enter crop START ROW (min=0, max={}: ".format(h)))
-  h1 = int(input("Enter crop END ROW (min=0, max={}: ".format(h)))
-  w0 = int(input("Enter crop START COLUMN (min=0, max={}: ".format(w)))
-  w1 = int(input("Enter crop END COLUMN (min=0, max={}: ".format(w)))
+  h0 = int(input("Enter crop START ROW (min=0, max={}): ".format(h)))
+  h1 = int(input("Enter crop END ROW (min=0, max={}): ".format(h)))
+  w0 = int(input("Enter crop START COLUMN (min=0, max={}): ".format(w)))
+  w1 = int(input("Enter crop END COLUMN (min=0, max={}): ".format(w)))
   
   h_min, h_max = min(h0, h1), max(h0, h1)
   w_min, w_max = min(w0, w1), max(w0, w1)
