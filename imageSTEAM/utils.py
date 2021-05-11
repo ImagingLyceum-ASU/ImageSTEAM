835d7ceed770a4de6aa1b867a310f28f00882c27
Joshua <jdrego@asu.edu>
2021-05-11 01:03:21 -0700

    Added dpi parameter to display function

--- imageSTEAM/utils.py
+++ imageSTEAM/utils.py
@@ -6,13 +6,15 @@ def test_func_utils():
     print("Test function in utils prints")
     
 # Displays an image, converts BGR to RGB
-def display_img(a, title = "Original"):
+def display_img(a, title = "Original", dpi=None):
+    if dpi != None:
+      plt.figure(dpi=dpi)
     plt.imshow(a, cmap='gray'), plt.title(title)
     plt.show()
     
 def crop(image, display=True):
   h, w = image.shape[:2]
-  print("Cropping image with original dimensions: \n{} rows x {} columns".format(h, w))
+  print("Cropping image with original dimensions: \n{} rows x {} columns\n".format(h, w))
   h0 = int(input("Enter crop START ROW (min=0, max={}): ".format(h)))
   h1 = int(input("Enter crop END ROW (min=0, max={}): ".format(h)))
   w0 = int(input("Enter crop START COLUMN (min=0, max={}): ".format(w)))
@@ -24,6 +26,6 @@ def crop(image, display=True):
   img_crop = image[h_min:h_max, w_min:w_max]
   
   if display == True:
-    display_img(img_crop)
+    display_img(img_crop, title='Cropped')
 
   return img_crop
