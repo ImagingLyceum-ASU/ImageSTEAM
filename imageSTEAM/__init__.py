529722da09da2a74abbe92dd80510cb121e675f1
Joshua <jdrego@asu.edu>
2021-05-11 01:07:35 -0700

    Updated with widgets module

--- imageSTEAM/__init__.py
+++ imageSTEAM/__init__.py
@@ -2,9 +2,11 @@
 from .base import *
 from .utils import *
 from .pypets import *
+from .widgets import *
 from . import base
 from . import utils
 from . import pypets
+from . import widgets
 # print('Test Package Import')
 
 # def test_func():
