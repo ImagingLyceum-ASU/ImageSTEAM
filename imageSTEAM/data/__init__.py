e81d00e3657511ff2bf8105f325653633318f0b5
Joshua <jdrego@asu.edu>
2021-05-12 18:22:06 -0700

    Bug fix

--- imageSTEAM/data/__init__.py
+++ imageSTEAM/data/__init__.py
@@ -4,4 +4,4 @@ import os.path as osp
 data_dir = osp.abspath(osp.dirname(__file__))
 
 def amongUs():
-  return io.imread(osp.join(data_dir, 'AmongUs.png'))
+  return io.imread(osp.join(data_dir, 'AmongUs_gray.png'))
