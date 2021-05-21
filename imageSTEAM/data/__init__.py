b6b320fad46276ae4d6345b980a0c66fd5801e5d
Joshua <jdrego@asu.edu>
2021-05-21 02:30:33 -0700

    Added fruits example image

--- imageSTEAM/data/__init__.py
+++ imageSTEAM/data/__init__.py
@@ -12,3 +12,6 @@ def headScan():
     headScan_1R = io.imread(osp.join(data_dir, 'headScan/HeadScan_1R.jpg'))
     headScan_2 = io.imread(osp.join(data_dir, 'headScan/HeadScan_2.jpg'))
     return [headScan_0, headScan_1L, headScan_1R, headScan_2]
+
+def fruits():
+    return io.imread(osp.join(data_dir, 'fruits.jpg'))
