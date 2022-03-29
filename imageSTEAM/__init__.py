be638bbed0c7878a8f4d5fee2bb7298a0cc181cd
sreenithy <sreenithy.chandran@gmail.com>
2022-03-29 09:14:45 -0700

    HDR

--- imageSTEAM/__init__.py
+++ imageSTEAM/__init__.py
@@ -4,12 +4,14 @@ from .utils import *
 from .pypets import *
 from .widgets import *
 from .color import *
+from .hdr import *
 from . import base
 from . import utils
 from . import pypets
 from . import widgets
 from . import data
 from . import color
+from . import hdr
 # print('Test Package Import')
 
 # def test_func():
