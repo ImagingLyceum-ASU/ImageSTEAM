25ce1382106422db441bd22ccd08fa3d5724c323
sreenithy c <sreenithyc21@gmail.com>
2022-06-07 13:27:42 +0530

    hsv range fix

--- imageSTEAM/hsv.py
+++ imageSTEAM/hsv.py
@@ -26,6 +26,7 @@ import imageio as io
 
 def pixelHSVExample(pixel):
     # pixel = #np.zeros((1,1,3), dtype='float32')
+    pixel = pixel.astype('float32') / 255.0
     pixel = rgb_to_hsv(pixel)
 
     sliderH = widgets.FloatSlider(description='Hue', value=0.5, min=0, max=1)
@@ -44,6 +45,43 @@ def pixelHSVExample(pixel):
     output = widgets.interactive_output(_update_display,
                                         {'h': sliderH, 's': sliderS, 'v': sliderV})
     final_widget = VBox([output, sliders])
+    display(final_widget)
+
+
+
+def HSVSegmentationrange(img):
+    img = img.astype('float32') / 255.0
+    img_segm = np.zeros_like(img, dtype='float32')
+    img_hsv = rgb_to_hsv(img)
+
+    segmented_out = widgets.Output()
+
+    sliderH = widgets.FloatRangeSlider(description='Hue', value=[0, 1.0], min=0, max=1, step=0.01)
+    sliderS = widgets.FloatRangeSlider(description='Saturation', value=[0, 1.0], min=0, max=1, step=0.01)
+    sliderV = widgets.FloatRangeSlider(description='Value', value=[0, 1.0], min=0, max=1, step=0.01)
+    sliders = VBox([sliderH, sliderS, sliderV])
+
+    def _update_display(h, s, v, img_segm=img_segm):
+        tmp_img = img_hsv.copy()
+        mask = (tmp_img[..., 0] >= h[0]) & (tmp_img[..., 0] <= h[1]) & (tmp_img[..., 1] >= s[0]) & (
+                    tmp_img[..., 1] <= s[1]) & (tmp_img[..., 2] >= v[0]) & (tmp_img[..., 2] <= v[1])
+        for c in range(img.shape[2]):
+            img_segm[..., c] = img[..., c] * mask
+
+        with segmented_out:
+            plt.imshow(img_segm, cmap='gray')
+            plt.show()
+            segmented_out.clear_output(wait=True)
+
+        img_segm = img
+
+    output = widgets.interactive_output(_update_display,
+                                        {'h': sliderH, 's': sliderS, 'v': sliderV})
+    with output:
+        plt.imshow(img);
+        plt.show()
+
+    final_widget = VBox([output, sliders, segmented_out])
     display(final_widget)
 
 
@@ -85,44 +123,3 @@ def HSVSegmentation(img):
     final_widget = VBox([output, sliders, segmented_out])
     display(final_widget)
 
-
-def HSVSegmentationrange(img):
-    img = img.astype('float32') / 255.0
-    img_segm = np.zeros_like(img, dtype='float32')
-    img_hsv = rgb_to_hsv(img)
-
-    segmented_out = widgets.Output()
-
-    sliderH = widgets.FloatRangeSlider(description='Hue', value=[0, 1.0], min=0, max=1, step=0.01)
-    sliderS = widgets.FloatRangeSlider(description='Saturation', value=[0, 1.0], min=0, max=1, step=0.01)
-    sliderV = widgets.FloatRangeSlider(description='Value', value=[0, 1.0], min=0, max=1, step=0.01)
-    sliders = VBox([sliderH, sliderS, sliderV])
-
-    def _update_display(h, s, v, img_segm=img_segm):
-        tmp_img = img_hsv.copy()
-        # for c in range(img.shape[2]):
-        #   tmp_img[..., c] = img[..., c] + [h, s, v][c]
-
-        # plt.imshow(hsv_to_rgb(tmp_img), cmap='gray')
-        # plt.show()
-
-        mask = (tmp_img[..., 0] >= h[0]) & (tmp_img[..., 0] <= h[1]) & (tmp_img[..., 1] >= s[0]) & (
-                    tmp_img[..., 1] <= s[1]) & (tmp_img[..., 2] >= v[0]) & (tmp_img[..., 2] <= v[1])
-        for c in range(img.shape[2]):
-            img_segm[..., c] = img[..., c] * mask
-
-        with segmented_out:
-            plt.imshow(img_segm, cmap='gray')
-            plt.show()
-            segmented_out.clear_output(wait=True)
-
-        img_segm = img
-
-    output = widgets.interactive_output(_update_display,
-                                        {'h': sliderH, 's': sliderS, 'v': sliderV})
-    with output:
-        plt.imshow(img);
-        plt.show()
-
-    final_widget = VBox([output, sliders, segmented_out])
-    display(final_widget)
