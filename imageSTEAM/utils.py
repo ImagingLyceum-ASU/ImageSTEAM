2c417e13129b61d9df360abf4760dd7989526066
Suren Jayasuriya <sjayasur@asu.edu>
2024-07-19 10:54:58 -0700

    Update utils.py
    Added ImageArray, read, flip, rotate classes and functions

--- imageSTEAM/utils.py
+++ imageSTEAM/utils.py
@@ -34,3 +34,52 @@ def crop(image, display=True):
         display_img(img_crop, title='Cropped')
 
     return img_crop
+
+import cv2
+import numpy as np
+
+class ImageArray(np.ndarray):
+
+    def __new__(cls, input_array):
+        # Input array is an already formed ndarray instance
+        # We first cast to be our class type
+        obj = np.asarray(input_array).view(cls)
+        # add the new attribute to the created instance
+        obj.length = len(input_array)
+        if obj.length >= 1:
+          obj.width = len(input_array[0])
+        # Finally, we must return the newly created object:
+        return obj
+
+    def __array_finalize__(self, obj):
+        # see InfoArray.__array_finalize__ for comments
+        if obj is None: return
+        self.info = getattr(obj, 'info', None)
+
+def read(name):
+  img = cv2.imread(name)
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  return ImageArray(img)
+
+def flip(src, direction='leftright'):
+  if direction == 'leftright':
+    print("true")
+    fd = 1
+    title = 'Left/Right'
+  else:
+    fd = 0
+    title = 'Up/Down'
+  display_img(cv2.flip(src, fd), title=title)
+
+# Rotates the image
+def rotate(rot, angle, origin, scale=1):
+  pt_y = int(origin[1])
+  pt_x = int(origin[0])
+
+  rows = int(len(rot) * scale)
+  cols = int(len(rot[0]) * scale)
+
+  # rotation - get proper rc length for output?
+  M = cv2.getRotationMatrix2D((pt_x,pt_y),angle,scale)
+  dst = cv2.warpAffine(rot,M,(cols,rows))
+  display_img(dst, title='Rotated ' + str(angle) + ' Degrees')
